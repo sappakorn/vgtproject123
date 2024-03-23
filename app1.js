e7faa5cc5a389f79e16b485631d9f0fcdb3277d5
Sappakorn26256 <sappakorn26256@gmail.com>
2024-03-23 19:25:40 +0700

    Update app1.js

--- app1.js
+++ app1.js
@@ -3,12 +3,6 @@ const mysql = require('mysql')
 const app = express();
 const port = 3000
 
-app.set('view engine' , 'ejs')
-app.get('/',(req,res)=>{
-  res.render('pages/index')
-})
-
-
 var con = mysql.createConnection({
   host: "node60666-vgtproject.th1.proen.cloud",
   user: "root",
@@ -21,5 +15,21 @@ con.connect(function(err) {
   console.log("You are connected!");
 });
 con.end(); 
+
+app.set('view engine' , 'ejs')
+
+app.get('/',(req,res)=>{
+  res.render('pages/index')
+  con.connect(function(err) {
+    if (err) throw err;
+    con.query("SELECT * FROM customers", function (err, result, fields) {
+      if (err) throw err;
+      console.log(result);
+    });
+  });
+  
+})
+
+
 
 app.listen(port, ()=> console.log('server is running on port 3000'));
