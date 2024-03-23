2764d6bed1ea82fae80d12612c18e57798aa8137
Sappakorn26256 <sappakorn26256@gmail.com>
2024-03-24 02:57:14 +0700

    s

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mysql = require('mysql');
 const ejs = require('ejs')
 const app = express();
-const port = 3000;
+const port = 3306;
 
 
 app.set('view engine', 'ejs');
