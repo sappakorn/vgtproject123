dd19cd21e8419f9b7cb79e0e10b71fa6963bc52a
Sappakorn26256 <sappakorn26256@gmail.com>
2024-03-24 02:35:27 +0700

    Update app.js
    asd

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mysql = require('mysql');
 const ejs = require('ejs')
 const app = express();
-const port = 3000;
+const port = 11991;
 const path = require('path')
 
 app.set('views', path.join(__dirname, 'views'));
@@ -12,7 +12,8 @@ var con = mysql.createConnection({
   host: "node60666-vgtproject.th1.proen.cloud:11991",
   user: "root",
   password: "OZIxac77163",
-  database: "myproject"
+  database: "myproject",
+  port:"3306"
 });
 
 con.connect(function (err) {
@@ -21,7 +22,7 @@ con.connect(function (err) {
   con.query("SELECT * FROM userprofile", function (error, result, fields) {
     if (error) throw error;
     console.log(result);
-    console.log("ejs ??? ")
+    
 
     app.get('/', (req, res) => {
       res.render('/pages/index', {
