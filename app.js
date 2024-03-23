4a1a4350d46ee7283b37679a69af929de52c2eab
Sappakorn26256 <sappakorn26256@gmail.com>
2024-03-24 02:29:04 +0700

    a

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const express = require('express');
 const mysql = require('mysql');
 const ejs = require('ejs')
 const app = express();
-const port = 11991;
+const port = 3000;
 const path = require('path')
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 var con = mysql.createConnection({
-  host: "node60666-vgtproject.th1.proen.cloud",
+  host: "node60666-vgtproject.th1.proen.cloud:11991",
   user: "root",
   password: "OZIxac77163",
   database: "myproject"
