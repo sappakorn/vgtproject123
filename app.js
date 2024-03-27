3b1474b46d4f5cd3052961e8b3492a1a7f236146
Sappakorn26256 <sappakorn26256@gmail.com>
2024-03-28 04:39:28 +0700

    a
    a

--- app.js
+++ app.js
@@ -1,35 +1,61 @@
 const express = require('express');
+const path = require('path');
 const mysql = require('mysql');
 const ejs = require('ejs')
 const app = express();
 const port = 11512;
+const bodyParser = require('body-parser');
 
+
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
 
+
+// Serve รูปภาพ ในไฟล์ public
+app.use(express.static('public'));
+//
+// Serve Bootstrap CSS โดยใช้ express.static()
+app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist'));
+//
+
+//เชื่อมต่อฐานข้อมูล
 var con = mysql.createConnection({
   host: "node60666-vgtproject.th1.proen.cloud",
   user: "root",
   password: "OZIxac77163",
   database: "myproject"
 });
+//
 
 app.get('/', function(req, res) {
-  const sql = "SELECT * FROM userprofile";
-  con.query(sql, function(err, result) {
-    if (err) throw err;
-    res.render('pages/index', { data: result });
-  });
+  
 });
+
+
 
 app.get('/register', function(req, res) {
   res.render('pages/register');
 });
 
-const sql = "INSERT INTO userprofile (username, password, fullname) VALUES ('spk1','111111','Kspk')";
-con.query(sql, function (err, result) {
-  if (err) throw err;
-  console.log("1 record inserted");
-});
+app.post('/auth_register', function(req,res,error){
+  const username = req.body.username;
+  const password = req.body.password;
+  const password2 = req.body.password2;
+  const phone_number = req.body.phone_number;
+  const fullname = req.body.fullname;
+  const sql = "INSERT INTO userprofile (username, password,password2,phone,fullname) VALUES (?,?,?,?,?)";
+  con.query(sql, [username, password,password2,phone_number, fullname], function (err, result) {
+    if (err) throw err;
+    res.render('pages/auth_register',{});
+
+  });
+  if(error){
+    console.log(error)
+  }
+})
+
+
 
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
