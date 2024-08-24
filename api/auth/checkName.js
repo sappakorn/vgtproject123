a1c3a2aec6d9ed63f9a05c2041f946ab94646418
sappakorn <sappakorn.ka@rmuti.ac.th>
2024-08-24 21:41:48 +0700

    Ne
    Name Split

--- api/auth/checkName.js
+++ api/auth/checkName.js
@@ -2,8 +2,47 @@ const express = require('express')
 const router = express.Router();
 const con = require('../../models/config/database');
 
-router.get('/', (req,res) => {
-    const receiverName = 'SAPPKORN K'
+router.get('/', (req, res) => {
+    //ฝั่งร้านค้า
+    const receiverName = 'สรรพกร แ'
+    const fisrtName = 'สรรพกร'
+    const lastName = 'แก่นนาคำ'
+
+    const nameSplit = receiverName.split(' ')
+    const receiverFName = nameSplit[0]
+    const receiverLName = nameSplit[1]
+    const isFisrtNameMath = receiverFName === fisrtName;
+    const idLastNameMath = receiverLName === lastName.charAt(0);
+    
+    
+    if(isFisrtNameMath && idLastNameMath){
+        console.log(receiverFName)
+        console.log(receiverLName)
+        console.log('receiverMath')
+    }
+    //
+
+    //ฝั่งลูกค้า
+    const senderName = 'ดาราทอง ล'
+    const customer_fname  = 'ดาราทอง'
+    const customer_lname = 'ลุนบง'
+
+    const customersplit = senderName.split(' ')
+    const senderFName = customersplit[0]
+    const senderLName = customersplit[1]
+    const isCustomerNameMath = senderFName === customer_fname;
+    const isCustomerLNameMath = senderLName === customer_lname.charAt(0)
+
+    if(isCustomerNameMath && isCustomerLNameMath){
+        console.log(senderFName)
+        console.log(senderLName)
+        console.log('senderMath')
+    }
+    //
+
+
+    res.send('Math = 2')
+
 })
 
 module.exports = router;
