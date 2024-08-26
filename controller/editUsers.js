18ab1cea0cd68857995e6ad7c8da96abf7f5302c
Anuphongla <anuphong.la@rmuti.ac.th>
2024-08-26 23:38:39 +0700

    Nu
    update

--- controller/editUsers.js
+++ controller/editUsers.js
@@ -1,29 +1,29 @@
 const express = require('express') //req res redirect 
 const router = express.Router()
+const con = require('../models/config/database')
 
 router.post('/', function (req, res) {
     
+    const id = req.body.user_id;
     const phone = req.body.phone;
     const first_name = req.body.first_name;
     const last_name = req.body.last_name;
-    const lo_shop = req.body.lo_shop;
+    const location_shop = req.body.location_shop;
     const name_shop = req.body.name_shop;
     const pp_number = req.body.pp_number;
-
-    console.log(phone+password+first_name+last_name+lo_shop+name_shop+pp_number)
+    
+    console.log(phone+first_name+last_name+location_shop+name_shop+pp_number)
     
     //update 
-    const sql = "  ";
-    con.query(sql,[id], (err,result)=>{
-
-      if(err){
-        res.send(err)
-      }else{
-        res.send('success')
+    const sql = `UPDATE usersprofile SET phone = ?, first_name = ?, last_name = ?, location_shop = ?, name_shop = ?, pp_number = ? WHERE user_id = ?`;
+    con.query(sql, [phone, first_name, last_name, location_shop, name_shop, pp_number, id], (err, result) => {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        console.log('err'+ err + result);
+        res.send('success');
       }
-      
-      
-    })
+    });
 })
 
 module.exports = router;
