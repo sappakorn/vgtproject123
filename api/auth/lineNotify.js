28157cc1cc48272a9b9cacdc292df72278033037
Anuphongla <anuphong.la@rmuti.ac.th>
2024-08-28 21:55:24 +0700

    ืnuuuu
    สลิป

--- api/auth/lineNotify.js
+++ api/auth/lineNotify.js
@@ -20,18 +20,48 @@ router.get('/', (req, res) =>  {
     
     const customer_amount = req.session.customer_amount //ยอดรวม
     const customer_name = req.session.customer.customer_name //ชื่อลค.
-    const customer_phone = req.session.customer.customer_phone //ชื่อลค.
+    const customer_phone = req.session.customer.customer_phone //เบอร์ลค.
     const currentList = req.session.currentList //รายการสินค้า 
+    const quantity = req.session.quantity //จำนวน
+    const productPrice = req.session.productPrice //ราคาต่อชิ้น
     
     console.log("phone"+customer_phone)
+    console.log("จำนวน"+quantity)
     console.log("ราคารวม"+customer_amount) 
     console.log("name"+customer_name)
+    console.log("ราคาต่อชิ้น"+productPrice)
     console.log(currentList)
 
     const client = new line.Client(config);
+    // const flexMessage = {
+    //     type: 'flex',
+    //     altText: 'รายการสั่งสินค้า',
+    //     contents: {
+    //         type: 'bubble',
+    //         body: {
+    //             type: 'box',
+    //             layout: 'vertical',
+    //             contents: [
+    //                 {
+    //                     type: 'text',
+    //                     text: 'มีรายการสั่งซื้อสินค้า',
+    //                     weight: 'bold',
+    //                     color: '#00ff4c',  // สีเขียว
+    //                     size: 'lg'
+    //                 },
+    //                 ...currentList.map((item, index) =>  ({
+    //                     type: 'text',
+    //                     text: `รายการที่${index+1} ${item.productName} (${item.productType}) จำนวน ${item.quantity} ราคา ${item.productPrice} บาท`,
+    //                     wrap: true,
+    //                     margin: 'md'
+    //                 }))
+    //             ]
+    //         }
+    //     }
+    // };
     const flexMessage = {
         type: 'flex',
-        altText: 'มีรายการสินใหม่เข้า',
+        altText: 'รายการสั่งสินค้า',
         contents: {
             type: 'bubble',
             body: {
@@ -40,17 +70,101 @@ router.get('/', (req, res) =>  {
                 contents: [
                     {
                         type: 'text',
-                        text: 'มีรายการสินใหม่เข้า',
+                        text: 'มีรายการสั่งซื้อสินค้า ',
                         weight: 'bold',
-                        color: '#00ff4c',  // สีเขียว
-                        size: 'lg'
+                        color: '#00b527',  // สีเขียว
+                        size: 'md'
                     },
-                    ...currentList.map((item, index) =>  ({
+                    {
                         type: 'text',
-                        text: `รายการที่${index+1} ${item.productName} (${item.productType}) จำนวน ${item.quantity} ราคา ${item.productPrice} บาท`,
-                        wrap: true,
-                        margin: 'md'
-                    }))
+                        text: ` คุณ ${customer_name}`, // ชื่อลูกค้า
+                        size: 'sm',
+                        color: '#888888',
+                        margin: 'sm'
+                    }
+                    ,
+                    {
+                        type: 'text',
+                        text: ` เบอร์โทร ${customer_phone}`, // เบอร์ลูกค้า
+                        size: 'sm',
+                        color: '#888888',
+                        margin: 'sm'
+                    },
+                    {
+                        type: 'separator',
+                        margin: 'xl'
+                    },
+                    {
+                        type: 'box',
+                        layout: 'vertical',
+                        margin: 'xl',
+                        spacing: 'sm',
+                        contents: [
+                            ...currentList.map((item, index) =>  ({
+                                type: 'box',
+                                layout: 'horizontal',
+                                contents: [
+                                    {
+                                        type: 'text',
+                                        text: `${item.productName}`,
+                                        size: 'sm',
+                                        color: '#555555',
+                                        flex: 1
+                                    },
+                                    {
+                                        type: 'text',
+                                        text: `${item.quantity} ${item.productType}`,
+                                        size: 'sm',
+                                        color: '#111111',
+                                        align: 'center'
+                                    },
+                                    {
+                                        type: 'text',
+                                        text: `${item.productPrice} บาท`,
+                                        size: 'sm',
+                                        color: '#111111',
+                                        align: 'end'
+                                    }
+                                ]
+                            })),
+                            {
+                                type: 'separator',
+                                margin: 'xl'
+                            },
+                            {
+                                type: 'box',
+                                layout: 'horizontal',
+                                contents: [
+                                    
+                                    {
+                                        type: 'text',
+                                        text: 'รวมเป็นเงิน',
+                                        size: 'md',
+                                        weight: 'bold',
+                                        color: '#555555',
+                                        flex: 1
+                                    },
+                                    {
+                                        type: 'text',
+                                        text: `${customer_amount} บาท`,
+                                        size: 'md',
+                                        weight: 'bold',
+                                        color: '#111111',
+                                        align: 'end'
+                                    }
+                                ]
+                                
+                            },
+                            {
+                                type: 'text',
+                                text: 'ชำระเงินแล้ว',
+                                size: 'sm',
+                                weight: 'bold',
+                                color: '#555555',
+                                align: 'end'
+                            }
+                        ]
+                    }
                 ]
             }
         }
