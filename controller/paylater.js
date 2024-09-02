ed30fe93a07bfa29fe0aae1ee3ad9ce3493f86c6
sappakorn <sappakorn.ka@rmuti.ac.th>
2024-09-02 15:36:21 +0700

    NE
    paylater

--- controller/paylater.js
+++ controller/paylater.js
@@ -1,10 +1,20 @@
 const express = require('express')
 const con = require('../models/config/database');
+const { cache } = require('ejs');
 const router = express.Router();
 
 router.get('/', (req, res) => {
+    const user_id = req.session.user.user_id;
     
-    res.render('pages/paylater',{})
+    try {
+        const sql = 'select '
+        res.render('pages/paylater',{
+
+        })
+    } catch (error) {
+        
+    }
+    
 })
 
 module.exports = router;
